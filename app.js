fe75e0e33b1c16d95bea1e3e79ecdf248caaaee3
unknown <aditya431>
2021-05-27 01:20:08 +0530

    middlewares

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const http= require('http');
-const route = require('./routes')
+const express = require('express');
+const { nextTick } = require('process');
+const app=express()
 
 
-const server = http.createServer(route)
+app.use((req,res,next)=>{
+    console.log("middleware")
+    next();
+})
+
+app.use((req,res,nxt)=>{
+    console.log("middleware2")
+    // res.send("<h1>hello</h1>")
+    res.send( { key1:"1"});    
+})
+
+
+const server = http.createServer(app)
 
 server.listen(4000,"localhost",()=>{
     console.log("listening");
